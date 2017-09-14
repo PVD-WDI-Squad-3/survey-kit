9f0efe2e734e48082a9eda750e252c75a61ec417
narfpoit <moongold81@gmail.com>
2017-09-14 13:53:12 -0400

    updated surveys controller

--- app/controllers/surveys.js
+++ app/controllers/surveys.js
@@ -1,12 +1,22 @@
 const controller = require('lib/wiring/controller')
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/survey-kit-back-end-development')
+const db = mongoose.connection
 const models = require('app/models')
 const Survey = models.survey
 
 const authenticate = require('./concerns/authenticate')
 const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
+const randUrl  = require('gyfcat-style-urls')
 
-const create = (req, res, next) => {
+const done = function () {
+  db.close()
+}
+
+/*const create = (req, res, next) => {
   const survey = Object.assign(req.body.survey, {
     _owner: req.user._id
   })
@@ -17,4 +27,24 @@ const create = (req, res, next) => {
           survey: survey.toJSON({ virtuals: true, user: req.user })
         }))
     .catch(next)
+}*/
+
+const create = function (title, content) {
+
+  let rand = randUrl.generateCombination(3, "_")
+  const surveyParams = {
+    title: title,
+    content: content,
+    completed: false,
+    url: rand
+  }
+
+  Survey.create(surveyParams)
+    .then(function(survey) {
+      console.log(survey)
+    })
+    .catch(function(error) {
+      console.error(error)
+    })
+    .then(done)
 }
