9d27d1bb6145348130fc04be1a7a3b6bb91ed1eb
narfpoit <moongold81@gmail.com>
2017-09-14 22:11:28 -0400

    updated survey logic

--- config/routes.js
+++ config/routes.js
@@ -15,7 +15,7 @@ module.exports = require('lib/wiring/routes')
 .post('/sign-in', 'users#signin')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
-.post('/surveys', 'surveys#create')
+.resources('surveys')
 .resources('users', { only: ['index', 'show'] })
 
 // all routes created
