88b1850c357b3023629c512b59577d57e0a8a55c
joseealejandro <joseealejandro@yahoo.com>
2017-09-15 16:45:25 -0400

    update delete actions in controller tested with CURL Script

--- app/controllers/surveys.js
+++ app/controllers/surveys.js
@@ -42,7 +42,9 @@ const show = (req, res) => {
 }
 
 const destroy = (req, res, next) => {
-
+  req.survey.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
 }
 
 const update = (req, res, next) => {
